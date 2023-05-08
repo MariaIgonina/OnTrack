5e52f40cacea0d6a3f92beb428690d2dbc8ad80e
Maria <igoninamv@yandex.ru>
2023-05-08 15:57:05 +0200

    FE: Added interfaces

---
+++ client/src/store/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import applicantReducer from "./userSlice"
+
+
+
+export const store = configureStore({
+  reducer: {
+    applicant: applicantReducer,
+  },
+})
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
