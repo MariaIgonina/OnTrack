8c0102b682b5858f675bca1832378b196e518776
Paola <paola.patino.castillo@gmail.com>
2023-05-09 13:15:38 +0200

    applicant filters working

--- server/controllersMain/applicantControllers.ts
+++ server/controllersMain/applicantControllers.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 
 import dotenv from 'dotenv';
+import { lang } from "moment";
+import { read } from "fs";
 dotenv.config()
 
 const prisma = new PrismaClient();
@@ -88,23 +90,75 @@ const deleteApplicant = async (req: Request, res: Response) => {
 }
 
 const filterApplicants = async (req: Request, res: Response) => {
-  const queries = req.query
-  // send the params in this order as a single string, each param separated by commas:
-  // http://localhost:3000/filterApplicants/salary,desiredlocation,workinghours
-  console.log(queries.salary)
-  console.log(queries.workinghours)
-  console.log(queries.languages)
+  const queryObj: any = {}
+  let {
+    salaryRange,
+    languages,
+    workingModal,
+    workingHours,
+    compLanguages,
+    readyMove,
+    stack,
+    skillsProf,
+    desiredLocation
+  } = req.query;
+
+  if (desiredLocation) {
+    queryObj.OR = [
+        { desiredLocation: { hasSome: String(desiredLocation).split(',') } },
+        { location: desiredLocation }
+    ]
+  }
+
+  if (salaryRange) {
+    queryObj.salaryRange = { lte: +salaryRange! }
+  }
+
+  if (languages) {
+    queryObj.languages = { hasSome: String(languages).split(',') }
+  }
+
+  if (workingModal) {
+    queryObj.workingModal = workingModal
+  }
+
+  if (workingHours) {
+    queryObj.workingHours = workingHours
+  }
+
+  if (compLanguages) {
+    queryObj.compLanguages = {
+      hasSome: String(compLanguages).split(','),
+    }
+  }
+
+  if (readyMove) {
+    queryObj.readyMove = Boolean(readyMove)
+  }
+
+  if (stack) {
+    queryObj.stack = {
+      hasSome: String(stack).split(','),
+    }
+  }
+  if (skillsProf) {
+    queryObj.skillsProf = {
+      hasSome: String(skillsProf).split(','),
+    }
+  }
+
+  console.log('current query ->', queryObj)
+
+
   try {
     const applicants = await prisma.applicant.findMany({
-      where: {
-        salaryRange: {
-          gte: +queries.salary!,
-        }
-      }
+      where: {...queryObj}
     })
-    res.json('all good')
-  } catch (error) {
-
+    if (!applicants.length) throw new Error('No matches found')
+    res.status(200).json(applicants)
+  } catch (error: any) {
+    console.log(error);
+    res.status(400).json(error.message)
   }
 }
 
