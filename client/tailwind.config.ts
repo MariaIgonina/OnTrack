3457192bf31ec0eb986ac4c3cdb8005746157d5f
Rosie Dyvig <rosiedyvig@gmail.com>
2023-05-09 21:07:01 +0200

    suggestion comp

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        "stone-100": "#FFFDED",
+        "green-100": "#568EA3",
+      },
+
+      // colors: {
+      //   blue: "#568EA3",
+      //   background: "FFFDED",
+      //   yellow: "FCE762",
+      //   orange: "FFB17A",
+      //   cream: "FFE8D1",
+      // },
+    },
   },
   plugins: [],
 };
