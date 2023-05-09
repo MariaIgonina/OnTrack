d9642c20f8dfdc5b04bfed9dcedf4d96b79526ef
Xavier Graells <xaviergraells@Xaviers-Air.lan>
2023-05-09 21:01:21 +0200

    populated

--- server/controllersMain/populateController.ts
+++ server/controllersMain/populateController.ts
@@ -1,19 +1,50 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 import { msg1, msg2, msg3 } from "../mockData/mockMessage";
-import { action1, action2, action3 } from "../mockData/mockAction";
+// import { action1, action2, action3 } from "../mockData/mockAction";
 import { vacancy1, vacancy2, vacancy3 } from "../mockData/mockVacancy";
+import { track1, track2, track3, track4 } from "../mockData/mockTracks";
+import { guillaume, paola, xavi } from "../mockData/mockApplicants";
+import {
+  Questionnary1,
+  Questionnary2,
+  Questionnary3,
+} from "../mockData/mockQuestionnary";
+import { recruiter1, recruiter2, recruiter3 } from "../mockData/mockRecruiter";
+import { step1, step2, step3 } from "../mockData/mockStep";
 
-const { stepId: stepId1, ...actionData1 } = action1;
-const { stepId: stepId2, ...actionData2 } = action2;
-const { stepId: stepId3, ...actionData3 } = action3;
+// const { stepId: stepId1, ...actionData1 } = action1;
+// const { stepId: stepId2, ...actionData2 } = action2;
+// const { stepId: stepId3, ...actionData3 } = action3;
 
 const { trackId: trackId1, ...msgData1 } = msg1;
 const { trackId: trackId2, ...msgData2 } = msg2;
 const { trackId: trackId3, ...msgData3 } = msg3;
 
 async function populateDatabase() {
-  //populate vacancy
+  //applicant
+  try {
+    const applicantA = await prisma.applicant.create({ data: { ...xavi } });
+  } catch (error) {
+    console.log(error);
+  }
+  const applicantB = await prisma.applicant.create({ data: { ...guillaume } });
+  const applicantC = await prisma.applicant.create({ data: { ...paola } });
+  // recruiter
+  try {
+    const recruiterA = await prisma.recruiter.create({
+      data: { ...recruiter1 },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  const recruiterB = await prisma.recruiter.create({
+    data: { ...recruiter2 },
+  });
+  const recruiterC = await prisma.recruiter.create({
+    data: { ...recruiter3 },
+  });
+  //populate
   try {
     const vacancyA = await prisma.vacancy.create({
       data: {
@@ -36,43 +67,132 @@ async function populateDatabase() {
       recruiter: { connect: { id: vacancy3.recruiterId } },
     },
   });
-  //populate action
+  // track
   try {
-    const actionA = await prisma.action.create({
+    const trackA = await prisma.track.create({
       data: {
-        ...actionData1,
-        action: { connect: { id: stepId1 } },
+        ...track1.data,
+        Recruiter: { connect: { id: track1.recruiterID } },
+        Applicant: { connect: { idDB: track1.applicantID } },
+        Vacancy: { connect: { id: track1.vacancyId } },
       },
     });
   } catch (error) {
     console.log(error);
   }
-  const actionB = await prisma.action.create({
+
+  const trackB = await prisma.track.create({
     data: {
-      ...actionData2,
-      action: { connect: { id: stepId2 } },
+      ...track2.data,
+      Recruiter: { connect: { id: track2.recruiterID } },
+      Applicant: { connect: { idDB: track2.applicantID } },
+      Vacancy: { connect: { id: track2.vacancyId } },
     },
   });
-  const actionC = await prisma.action.create({
+  const trackC = await prisma.track.create({
     data: {
-      ...actionData3,
-      action: { connect: { id: stepId3 } },
+      ...track3.data,
+      Recruiter: { connect: { id: track3.recruiterID } },
+      Applicant: { connect: { idDB: track3.applicantID } },
+      Vacancy: { connect: { id: track3.vacancyId } },
     },
   });
-  //populate messge
+  const trackD = await prisma.track.create({
+    data: {
+      ...track4.data,
+      Recruiter: { connect: { id: track4.recruiterID } },
+      Applicant: { connect: { idDB: track4.applicantID } },
+      Vacancy: { connect: { id: track4.vacancyId } },
+    },
+  });
+  // messge
   try {
     const msgA = await prisma.message.create({
-      data: { ...msgData1, track: { connect: { id: trackId1 } } },
+      data: {
+        ...msgData1,
+        date: new Date(msgData1.date),
+        track: { connect: { id: trackId1 } },
+      },
     });
   } catch (error) {
     console.log(error);
   }
   const msgB = await prisma.message.create({
-    data: { ...msgData2, track: { connect: { id: trackId2 } } },
+    data: {
+      ...msgData2,
+      date: new Date(msgData2.date),
+      track: { connect: { id: trackId1 } },
+    },
   });
   const msgC = await prisma.message.create({
-    data: { ...msgData3, track: { connect: { id: trackId3 } } },
+    data: {
+      ...msgData3,
+      date: new Date(msgData3.date),
+      track: { connect: { id: trackId1 } },
+    },
   });
+  //step
+  try {
+    const stepA = await prisma.step.create({
+      data: { ...step1.data, Track: { connect: { id: step1.trackId } } },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  const stepB = await prisma.step.create({
+    data: { ...step2.data, Track: { connect: { id: step2.trackId } } },
+  });
+  const stepC = await prisma.step.create({
+    data: { ...step3.data, Track: { connect: { id: step3.trackId } } },
+  });
+
+  //questionnary
+  try {
+    const questionA = await prisma.questionary.create({
+      data: {
+        ...Questionnary1.data,
+        Step: { connect: { id: Questionnary1.stepId } },
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  const questionB = await prisma.questionary.create({
+    data: {
+      ...Questionnary2.data,
+      Step: { connect: { id: Questionnary2.stepId } },
+    },
+  });
+  const questionC = await prisma.questionary.create({
+    data: {
+      ...Questionnary3.data,
+      Step: { connect: { id: Questionnary3.stepId } },
+    },
+  });
+
+  //populate action
+  //   try {
+  //     const actionA = await prisma.action.create({
+  //       data: {
+  //         ...actionData1,
+  //         action: { connect: { id: stepId1 } },
+  //       },
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  //   const actionB = await prisma.action.create({
+  //     data: {
+  //       ...actionData2,
+  //       action: { connect: { id: stepId2 } },
+  //     },
+  //   });
+  //   const actionC = await prisma.action.create({
+  //     data: {
+  //       ...actionData3,
+  //       action: { connect: { id: stepId3 } },
+  //     },
+  //   });
 }
 
 export { populateDatabase };
--- server/controllersMain/recruiterController.ts
+++ server/controllersMain/recruiterController.ts
@@ -7,11 +7,11 @@ export const recruiterController = {
   getRecruiterbyId,
   deleteRecruiterbyId,
   updateRecruiterbyId,
-  createRecruiter
+  createRecruiter,
 };
 
 async function getRecruiterbyId(req: Request, res: Response) {
-    const {id}  = req.params;
+  const { id } = req.params;
   try {
     const recruiter = await prisma.recruiter.findUnique({
       where: {
@@ -19,7 +19,7 @@ async function getRecruiterbyId(req: Request, res: Response) {
       },
     });
     res.json(recruiter).status(200);
-} catch (error: any) {
+  } catch (error: any) {
     console.log("error in recruiterController, ", error);
     res.status(400).json(error.message);
   }
@@ -34,7 +34,7 @@ async function deleteRecruiterbyId(req: Request, res: Response) {
       },
     });
     res.json(recruiter).status(204);
-} catch (error: any) {
+  } catch (error: any) {
     console.log(error);
     if (error.meta.cause === "Record to delete does not exist")
       res.status(404).json(error.meta.cause);
@@ -43,8 +43,8 @@ async function deleteRecruiterbyId(req: Request, res: Response) {
 }
 
 async function updateRecruiterbyId(req: Request, res: Response) {
-    const { id } = req.params;
-    const { name, logo, founded, about, externalLinks, headOffice } = req.body;
+  const { id } = req.params;
+  const { name, logo, founded, about, externalLinks, headOffice } = req.body;
   try {
     const recruiter = await prisma.recruiter.update({
       where: {
@@ -60,28 +60,33 @@ async function updateRecruiterbyId(req: Request, res: Response) {
       },
     });
     res.json(recruiter).status(200);
-} catch (error: any) {
+  } catch (error: any) {
     console.log(error);
-    if (error.meta.cause === 'Record to update does not exist') res.status(404).json(error.meta.cause)
+    if (error.meta.cause === "Record to update does not exist")
+      res.status(404).json(error.meta.cause);
     else res.status(409).json(error.meta.cause);
   }
 }
 
 async function createRecruiter(req: Request, res: Response) {
-    try {
-      const recruiter = await prisma.recruiter.create({
-        data: {
-          name: req.body.name,
-          logo: req.body.logo,
-          founded: req.body.founded,
-          about: req.body.about,
-          externalLinks: req.body.externalLinks,
-          headOffice: req.body.headOffice,
-        },
-      });
-      res.json(recruiter).status(201);
-    } catch (error: any) {
-        console.log(error);
-        res.status(404).json(error.message);
-      }
+  try {
+    const recruiter = await prisma.recruiter.create({
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+        picture: req.body.picture,
+        idAuth: req.body.idAuth,
+        recruiterName: req.body.recruiterName,
+        logo: req.body.logo,
+        founded: req.body.founded,
+        about: req.body.about,
+        externalLinks: req.body.externalLinks,
+        headOffice: req.body.headOffice,
+      },
+    });
+    res.json(recruiter).status(201);
+  } catch (error: any) {
+    console.log(error);
+    res.status(404).json(error.message);
   }
+}
