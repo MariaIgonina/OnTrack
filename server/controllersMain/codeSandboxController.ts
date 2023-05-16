49d4e20299014c3fc11017def6e5b17440fc4687
GuillaumeRcd <120723419+GuillaumeRcd@users.noreply.github.com>
2023-05-16 09:33:26 +0200

    before merging

--- server/controllersMain/codeSandboxController.ts
+++ server/controllersMain/codeSandboxController.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from "express";
 import axios from 'axios'
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
 async function sendCompile(req: Request, res: Response) {
   const { language_id, source_code, customInput } = req.body;
   console.log('req.body BE => ', req.body)
@@ -34,6 +38,18 @@ console.log('lang -> ', formData.language_id, typeof formData.language_id)
       let error = err.response ? err.response.data : err;
       console.log('error from BE axios ->', error);
     });
+}
+
+const createSandox = async (req:Request, res:Response)=>{
+  // try {
+
+  //   const codebox = await prisma.codeSandbox.create({
+
+  //   })
+    
+  // } catch (error) {
+    
+  // }
 }
 
 export const codeSandbox = {
