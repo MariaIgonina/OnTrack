c07fb31919225346185b5e2a2d90566cac996e22
Rosie Dyvig <rosiedyvig@gmail.com>
2023-05-16 12:04:58 +0200

    modified update/create recruiter account

--- client/src/Pages/SettingsPage.tsx
+++ client/src/Pages/SettingsPage.tsx
@@ -7,6 +7,8 @@ import RecruiterForm from "../Components/RecruiterForm";
 import DashboardPage from "./DashboardPage";
 import { fetchApplicant, setApplicant } from "../store/applicantSlice";
 import stock4 from "../assets/stock-4.jpeg";
+import { uploadImage } from "../api.cloudinary";
+import { useNavigate } from "react-router-dom";
 
 import {
   fetchRecruiter,
@@ -16,12 +18,18 @@ import {
 } from "../store/recruiterSlice";
 
 export default function SettingsPage() {
+  const navigate = useNavigate();
   const recruiter = useSelector((state: RootState) => state.recruiter);
   const dispatch = useDispatch<AppDispatch>();
   const applicant = useSelector((state: RootState) => state.applicant);
-
+  const [externalLinks, setExternalLinks] = useState<(string | undefined)[]>(
+    []
+  );
+  const [previewSource, setPreviewSource] = useState<
+    string | ArrayBuffer | null
+  >();
   const currentUser = useSelector((s: RootState) => s.currentUser);
-
+  const [formData, setFormData] = useState(recruiter.recruiter);
   const codeParam = window.location.pathname.split("/").reverse()[0];
 
   useEffect(() => {
@@ -31,6 +39,10 @@ export default function SettingsPage() {
       dispatch(fetchApplicant(+currentUser.id));
     }
   }, [dispatch]);
+
+  const onCancel = () => {
+    navigate(`/${currentUser.role}/${currentUser.id}`);
+  };
 
   if (currentUser.role === "applicant") {
     return <AddApplicantPage />;
@@ -42,9 +54,14 @@ export default function SettingsPage() {
             src={stock4}
             alt="stockphoto"
             style={{ width: "90%", height: 550 }}
-            className="my-8"
+            className="my-8 flex-1"
           />
-          <RecruiterForm />
+          <div className="flex-1">
+            <h1 className="text-3xl font-bold tracking-tight text-[#026767] text-big text-center">
+              Update your account details
+            </h1>
+            <RecruiterForm />
+          </div>
         </div>
       </div>
     );
--- client/src/Pages/UserLogin.tsx
+++ client/src/Pages/UserLogin.tsx
@@ -5,17 +5,26 @@ import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../store/store";
 import DashboardPage from "./DashboardPage";
 import { fetchApplicant, setApplicant } from "../store/applicantSlice";
-import { fetchRecruiter, setRecruiter, createRecruiter, updateRecruiter } from "../store/recruiterSlice";
+import {
+  fetchRecruiter,
+  setRecruiter,
+  createRecruiter,
+  updateRecruiter,
+} from "../store/recruiterSlice";
 import { useNavigate } from "react-router-dom";
 import RecruiterForm from "../Components/RecruiterForm";
 
 // There's some thing strange with the type, we have to check, probably in the slice.
 
 export default function RecruiterLogin() {
-  const recruiter = useSelector((state: RootState) => state.recruiter.recruiter);
+  const recruiter = useSelector(
+    (state: RootState) => state.recruiter.recruiter
+  );
   const dispatch = useDispatch<AppDispatch>();
-  const applicant = useSelector((state: RootState) => state.applicant.applicant);
-  const navigate = useNavigate()
+  const applicant = useSelector(
+    (state: RootState) => state.applicant.applicant
+  );
+  const navigate = useNavigate();
 
   const currentUser = useSelector((s: RootState) => s.currentUser);
 
@@ -23,11 +32,11 @@ export default function RecruiterLogin() {
 
   useEffect(() => {
     console.log("inside the user login page", currentUser);
-    currentUser.role === undefined && navigate('/');
+    currentUser.role === undefined && navigate("/");
   }, []);
 
   useEffect(() => {
-    console.log('currentUser => ', currentUser?.role, currentUser)
+    console.log("currentUser => ", currentUser?.role, currentUser);
     if (currentUser.role !== "applicant") {
       // dispatch(setRecruiter(currentUser));
       dispatch(fetchRecruiter(+currentUser.id!));
@@ -35,20 +44,40 @@ export default function RecruiterLogin() {
       // dispatch(setApplicant(recruiter));
       dispatch(fetchApplicant(+currentUser.id!));
     }
-    console.log(recruiter)
+    console.log(recruiter);
   }, [dispatch]);
 
   return (
     <>
-      {currentUser.role !== undefined ?
-        currentUser?.role === "applicant"
-          ? applicant?.familyName ? <DashboardPage /> : <AddApplicantPage />
-          : recruiter?.name ? <DashboardPage /> : <RecruiterForm />
-        :
-        currentUser.role === undefined &&
-        <div className="w-screen h-screen
-        flex justify-center items-center text-lg text-green-800 bg-stone-100">Redirecting to login</div>
-      }
+      {currentUser.role !== undefined ? (
+        currentUser?.role === "applicant" ? (
+          applicant?.familyName ? (
+            <DashboardPage />
+          ) : (
+            <AddApplicantPage />
+          )
+        ) : recruiter?.name ? (
+          <DashboardPage />
+        ) : (
+          <>
+            <div className="p-2 flex items-center justify-center mt-2">
+              <h1 className="text-3xl font-bold tracking-tight text-[#026767] text-big  ">
+                Create the recruiter account
+              </h1>
+            </div>
+            <RecruiterForm />
+          </>
+        )
+      ) : (
+        currentUser.role === undefined && (
+          <div
+            className="w-screen h-screen
+        flex justify-center items-center text-lg text-green-800 bg-stone-100"
+          >
+            Redirecting to login
+          </div>
+        )
+      )}
     </>
-  )
+  );
 }
