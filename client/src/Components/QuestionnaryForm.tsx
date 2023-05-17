95b02cf4b20b4a71095df5347d049aca81af304d
GuillaumeRcd <120723419+GuillaumeRcd@users.noreply.github.com>
2023-05-17 10:38:11 +0200

    need styling

--- client/src/Components/QuestionnaryForm.tsx
+++ client/src/Components/QuestionnaryForm.tsx
@@ -56,7 +56,7 @@ export default function QuestionnaryForm({ step }: QuestionaryFormProps) {
     dispatch(updateQuestionary(updatedQuestionnary));
     // console.log(questionary);
     setDisplay(false);
-    window.location.reload()
+    window.location.reload();
   };
 
   return (
@@ -144,7 +144,9 @@ export default function QuestionnaryForm({ step }: QuestionaryFormProps) {
 
               {step.answer.length !== 0 && (
                 <div id="accordionExample">
-                  <div className="rounded-t-lg border border-neutral-200 bg-white dark:border-neutral-600 dark:bg-neutral-800">
+                  <div 
+                  className="rounded-t-lg border border-neutral-200 bg-stone-100"
+                  style={{background: "FFFDED"}}>
                     <h2 className="mb-0" id="headingTwo">
                       <button
                         className="group relative flex w-full items-center rounded-t-[15px] border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white [&:not([data-te-collapse-collapsed])]:bg-white [&:not([data-te-collapse-collapsed])]:text-primary [&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:[&:not([data-te-collapse-collapsed])]:bg-neutral-800 dark:[&:not([data-te-collapse-collapsed])]:text-primary-400 dark:[&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(75,85,99)]"
@@ -155,8 +157,8 @@ export default function QuestionnaryForm({ step }: QuestionaryFormProps) {
                         aria-expanded="false"
                         aria-controls="collapseTwo"
                       >
-                        Accordion Item #2
-                        <span className="-mr-1 ml-auto h-5 w-5 shrink-0 rotate-[-180deg] fill-[#336dec] transition-transform duration-200 ease-in-out group-[[data-te-collapse-collapsed]]:mr-0 group-[[data-te-collapse-collapsed]]:rotate-0 group-[[data-te-collapse-collapsed]]:fill-[#212529] motion-reduce:transition-none dark:fill-blue-300 dark:group-[[data-te-collapse-collapsed]]:fill-white">
+                          <div className="text-2xl">Questionary</div>
+                          <span className="-mr-1 ml-auto h-5 w-5 shrink-0 rotate-[-180deg] fill-[#336dec] transition-transform duration-200 ease-in-out group-[[data-te-collapse-collapsed]]:mr-0 group-[[data-te-collapse-collapsed]]:rotate-0 group-[[data-te-collapse-collapsed]]:fill-[#212529] motion-reduce:transition-none dark:fill-blue-300 dark:group-[[data-te-collapse-collapsed]]:fill-white">
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
@@ -187,17 +189,15 @@ export default function QuestionnaryForm({ step }: QuestionaryFormProps) {
                             (question, index) => (
                               <div key={index} className="mt-3">
                                 <div className="text-white">{question}</div>
-                                <textarea
-                                  placeholder="Your answer here..."
-                                  className="w-full py-2 px-3 rounded-lg border border-color-#F1F0EA mt-1 focus:outline-none focus:ring-2 focus:ring-#568EA3 focus:border-transparent"
-                                  // type="text"
-                                  id="about"
-                                  name="about"
-                                  value={answers[index] || ""}
-                                  onChange={(event) =>
-                                    handleChange(index, event.target.value)
-                                  }
-                                />
+                                <div
+                                  className="w-full py-2 px-3 rounded-lg border border-color-#F1F0EA mt-1"
+                                  style={{
+                                    background: "#568EA3",
+                                    color: "white",
+                                  }}
+                                >
+                                  {answers[index] || ""}
+                                </div>
                               </div>
                             )
                           )}
@@ -206,7 +206,7 @@ export default function QuestionnaryForm({ step }: QuestionaryFormProps) {
                   </div>
                   <div className="flex justify-center">
                     <div
-                      style={{width: "176px", height: "48px"}}
+                      style={{ width: "176px", height: "48px" }}
                       className="mt-4 ml-4 mb-6 bg-green-700 rounded-lg shadow-xl flex items-center justify-center text-white font-medium"
                     >
                       Form Submitted
